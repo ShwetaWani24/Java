a0f6211dc8c246fa6b4760021f11e1b664933662
ShwetaWani24 <shwetawani00@gmail.com>
2024-09-04 17:50:20 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Program { 
+    public static void main(String[] args) {
+        int n = 6; 
+
+ for (int i = 1; i <= n; i++) {
+            
+  for (int j = i; j < n; j++) {
+    System.out.print(" ");
+      }
+            
+    for (int j = 1; j <= (2 * i - 1); j++) {
+      if (j == 1 || j == (2 * i - 1) || i == 6) {
+   System.out.print("*");
+ } else {
+       System.out.print(" ");
+         }
+            }
+            
+    System.out.println();
+     }
+  }
+}
+
