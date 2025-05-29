a31fcaeca38d9648b2eb3663f041b1c0b847f386
ShwetaWani24 <shwetawani00@gmail.com>
2025-05-29 15:19:44 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,97 +1,33 @@
-using System;
-using System.Data;
-using System.Data.SqlClient;
-using System.Windows;
+<Window x:Class="CrudWpfApp.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="CRUD Operation in C# (WPF)" Height="450" Width="700">
+    <Grid Margin="10">
+        <Label Content="CRUD Operation in C# (WPF)" FontSize="16" HorizontalAlignment="Center" Grid.Row="0" Margin="0,0,0,10"/>
 
-namespace WpfAdoApp
-{
-    public partial class MainWindow : Window
-    {
-        private readonly string connectionString = "Server=localhost\\SQLEXPRESS;Database=WpfAdoAppDb;Trusted_Connection=True;";
+        <StackPanel Orientation="Vertical" HorizontalAlignment="Left" VerticalAlignment="Top">
+            <Label Content="Name"/>
+            <TextBox x:Name="txtName" Width="200"/>
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            LoadPeople();
-        }
+            <Label Content="Age"/>
+            <TextBox x:Name="txtAge" Width="200"/>
 
-        private void LoadPeople()
-        {
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connectionString))
-                {
-                    string query = "SELECT * FROM People ORDER BY LastModified DESC";
-                    SqlDataAdapter adapter = new SqlDataAdapter(query, con);
-                    DataTable dt = new DataTable();
-                    adapter.Fill(dt);
-                    PeopleDataGrid.ItemsSource = dt.DefaultView;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error loading people: " + ex.Message);
-            }
-        }
+            <Label Content="Gender"/>
+            <TextBox x:Name="txtGender" Width="200"/>
 
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(NameTextBox.Text))
-            {
-                MessageBox.Show("Please enter a name.");
-                return;
-            }
+            <Label Content="City"/>
+            <TextBox x:Name="txtCity" Width="200"/>
+        </StackPanel>
 
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connectionString))
-                {
-                    con.Open();
-                    string query = "INSERT INTO People (Name, LastModified) VALUES (@Name, GETDATE())";
-                    SqlCommand cmd = new SqlCommand(query, con);
-                    cmd.Parameters.AddWithValue("@Name", NameTextBox.Text.Trim());
-                    cmd.ExecuteNonQuery();
-                }
+        <DataGrid x:Name="dataGrid" HorizontalAlignment="Right" Width="400" Height="250"
+                  AutoGenerateColumns="True" Margin="10,10,0,10" SelectionMode="Single"
+                  VerticalAlignment="Top" />
 
-                NameTextBox.Clear();
-                LoadPeople();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error adding person: " + ex.Message);
-            }
-        }
-
-        private void Delete_Click(object sender, RoutedEventArgs e)
-        {
-            if (PeopleDataGrid.SelectedItem == null)
-            {
-                MessageBox.Show("Please select a row to delete.");
-                return;
-            }
-
-            DataRowView row = PeopleDataGrid.SelectedItem as DataRowView;
-            int id = Convert.ToInt32(row["Id"]);
-
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connectionString))
-                {
-                    con.Open();
-                    string query = "DELETE FROM People WHERE Id = @Id";
-                    SqlCommand cmd = new SqlCommand(query, con);
-                    cmd.Parameters.AddWithValue("@Id", id);
-                    cmd.ExecuteNonQuery();
-                }
-
-                LoadPeople();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error deleting person: " + ex.Message);
-            }
-        }
-    }
-}
-
-
+        <StackPanel Orientation="Horizontal" HorizontalAlignment="Center" VerticalAlignment="Bottom" Margin="0,10,0,0">
+            <Button Content="Insert Record" Width="120" Margin="5" Background="#D96666" Click="Insert_Click"/>
+            <Button Content="Update Record" Width="120" Margin="5" Background="#D96666" Click="Update_Click"/>
+            <Button Content="Delete Record" Width="120" Margin="5" Background="#D96666" Click="Delete_Click"/>
+            <Button Content="Clear Data" Width="120" Margin="5" Background="#D96666" Click="Clear_Click"/>
+        </StackPanel>
+    </Grid>
+</Window>
